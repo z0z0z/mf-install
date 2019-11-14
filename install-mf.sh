9f93b42daea733fd948636158f35ed3f57950202
Alexander Vereeken <37543796+Alexander88207@users.noreply.github.com>
2019-11-14 22:10:11 +0100

    Support only 32bit/64bit Prefixes
    Yeah some systems like OpenBSD or FreeBSD have only 64bit or only 32bit.

--- install-mf.sh
+++ install-mf.sh
@@ -11,7 +11,31 @@ overrideDll() {
 scriptdir=$(dirname "$0")
 cd "$scriptdir"
 
+if [ -d "$WINEPREFIX/drive_c/windows/syswow64" ]; then
+
 cp -v syswow64/* "$WINEPREFIX/drive_c/windows/syswow64"
+
+overrideDll "mf"
+overrideDll "mferror"
+overrideDll "mfplat"
+overrideDll "mfplay"
+overrideDll "mfreadwrite"
+overrideDll "msmpeg2adec"
+overrideDll "msmpeg2vdec"
+overrideDll "sqmapi"
+
+export WINEDEBUG="-all"
+
+wine64 start regedit.exe mf.reg
+wine64 start regedit.exe wmf.reg
+
+wine64 regsvr32 msmpeg2vdec.dll
+wine64 regsvr32 msmpeg2adec.dll
+wine64 regsvr32 colorcnv.dll
+fi
+
+if [ -d "$WINEPREFIX/drive_c/windows/system32" ]; then
+
 cp -v system32/* "$WINEPREFIX/drive_c/windows/system32"
 
 overrideDll "mf"
@@ -27,13 +51,8 @@ export WINEDEBUG="-all"
 
 wine start regedit.exe mf.reg
 wine start regedit.exe wmf.reg
-wine64 start regedit.exe mf.reg
-wine64 start regedit.exe wmf.reg
-
-wine64 regsvr32 msmpeg2vdec.dll
-wine64 regsvr32 msmpeg2adec.dll
-wine64 regsvr32 colorcnv.dll
 
 wine regsvr32 msmpeg2vdec.dll
 wine regsvr32 msmpeg2adec.dll
 wine regsvr32 colorcnv.dll
+fi
